e8b5b2452d8498184a22a661e7f1e57974b05319
juang-10 <63117558+juang-10@users.noreply.github.com>
2021-09-14 23:26:04 -0500

    layout and display of pokemon information

---
+++ pokeapi-pokedex/src/components/Pokemon.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Pokemon = (props) => {
+    const { pokemon } = props; 
+    return (
+        <div className="pokemon-card">
+            <div className="pokemon-img">
+                <img src={pokemon.sprites.front_default} alt="pokemon.name"/>
+            </div>
+            <div>
+                <div>
+                    <h3>{pokemon.name}</h3>
+                    <div># {pokemon.id}</div>
+                </div>
+                <div>
+                    <div>
+                        {pokemon.types.map((type, idx) => {
+                            return (
+                                <div key={idx}>{type.name}</div>
+                            )
+                        })}
+                    </div>
+                    <div>♥️</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Pokemon;
