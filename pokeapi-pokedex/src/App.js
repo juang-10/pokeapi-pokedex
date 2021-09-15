ee4467cf845261a7db99a073fa9b974aa920193c
juang-10 <63117558+juang-10@users.noreply.github.com>
2021-09-14 21:16:50 -0500

    obtain all pokemon in console

--- pokeapi-pokedex/src/App.js
+++ pokeapi-pokedex/src/App.js
@@ -1,16 +1,32 @@
+import React from "react";
 import './App.css';
 import Navbar from './components/Navbar';
+import Pokedex from './components/Pokedex';
 import Searchbar from './components/Searchbar';
+import { getPokemons } from "./api";
 
-function App() {
+const {useState, useEffect} = React;
+
+export default function App() {
+  const [pokemons, setPokemons] = useState([]);
+  
+  const fetchPokemons = async () => {
+    try {
+      const data = await getPokemons();
+      console.log(data);
+    } catch (err) {}
+  }
+
+  useEffect(() => {
+    fetchPokemons();
+  }, [])
   return (
     <div>
       <Navbar/>
       <div className="App">
         <Searchbar/>
+        <Pokedex/>
       </div>
     </div>
   );
 }
-
-export default App;
