cfecdfe77045ba58269ebc3fa9433dc9258a120f
juang-10 <63117558+juang-10@users.noreply.github.com>
2021-09-14 20:08:17 -0500

    search for pokemon and show some features

--- pokeapi-pokedex/src/components/Searchbar.js
+++ pokeapi-pokedex/src/components/Searchbar.js
@@ -4,12 +4,14 @@ const {useState} = React;
 
 const Searchbar = () => {
     const [search, setSearch] = useState('');
+    const[pokemon, setPokemon] = useState('');
+
     const onChange = (e) => {
         setSearch(e.target.value);
     }
     const onClick = async (e) => {
         const data = await searchPokemon(search);
-        console.log(data);
+        setPokemon(data);
     }
     
     return <div>
@@ -19,6 +21,15 @@ const Searchbar = () => {
         </div>
         <div>
             <button onClick={onClick}>Buscar</button>
+        </div>
+        <div>
+            {pokemon &&
+            <div>
+                <div>Nombre: {pokemon.name}</div>
+                <div>Peso: {pokemon.weight}</div>
+                <img src={pokemon.sprites.front_default}/>
+            </div>
+            }
         </div>
     </div>;
 }
