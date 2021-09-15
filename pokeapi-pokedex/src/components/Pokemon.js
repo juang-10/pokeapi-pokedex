03ae6581bfffd0786c3e2f0ebb8246910de03831
juang-10 <63117558+juang-10@users.noreply.github.com>
2021-09-14 23:57:39 -0500

    add classes for styles

--- pokeapi-pokedex/src/components/Pokemon.js
+++ pokeapi-pokedex/src/components/Pokemon.js
@@ -4,23 +4,25 @@ const Pokemon = (props) => {
     const { pokemon } = props; 
     return (
         <div className="pokemon-card">
-            <div className="pokemon-img">
-                <img src={pokemon.sprites.front_default} alt="pokemon.name"/>
+            <div className="pokemon-img-container">
+                <img src={pokemon.sprites.front_default} alt="pokemon.name" className="pokemon-img"/>
             </div>
-            <div>
-                <div>
+            <div className="card-body">
+                <div className="card-top">
                     <h3>{pokemon.name}</h3>
                     <div># {pokemon.id}</div>
                 </div>
-                <div>
-                    <div>
+                <div className="card-bottom">
+                    <div className="pokemon-type">
                         {pokemon.types.map((type, idx) => {
                             return (
-                                <div key={idx}>{type.name}</div>
+                                <div key={idx} className="pokemon-type-text">
+                                    {type.type.name}
+                                </div>
                             )
                         })}
                     </div>
-                    <div>♥️</div>
+                    <div className="pokemon-favorite">💙</div>
                 </div>
             </div>
         </div>
