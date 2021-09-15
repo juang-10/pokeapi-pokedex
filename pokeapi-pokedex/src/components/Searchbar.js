c03b2fca17123c1b8642eefe682b77813ab449c8
juang-10 <63117558+juang-10@users.noreply.github.com>
2021-09-14 19:58:27 -0500

    rename function and import searchpokemon

--- pokeapi-pokedex/src/components/Searchbar.js
+++ pokeapi-pokedex/src/components/Searchbar.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { searchPokemon } from "../api";
 const {useState} = React;
 
 const Searchbar = () => {
     const [search, setSearch] = useState('');
     const onChange = (e) => {
         setSearch(e.target.value);
+    }
+    const onClick = async (e) => {
+        const data = await searchPokemon(search);
+        console.log(data);
     }
     
     return <div>
@@ -13,7 +18,7 @@ const Searchbar = () => {
             />
         </div>
         <div>
-            {search}
+            <button onClick={onClick}>Buscar</button>
         </div>
     </div>;
 }
