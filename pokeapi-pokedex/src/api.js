22879b271ba8439a2f673ca1a9bb509b3555a4b9
juang-10 <63117558+juang-10@users.noreply.github.com>
2021-09-14 21:17:20 -0500

    obtain pokemons with limit and offset

--- pokeapi-pokedex/src/api.js
+++ pokeapi-pokedex/src/api.js
@@ -4,7 +4,14 @@ export const searchPokemon = async (pokemon) => {
         const response = await fetch(url);
         const data = await response.json();
         return data;
-    } catch (err) {
-        
-    }
+    } catch (err) {}
+};
+
+export const getPokemons = async(limit=10, offset=0) => {
+    try {
+        let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (err) {}
 }
