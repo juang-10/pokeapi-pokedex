d2f66315968e161bf067355ee096b67552efc4ad
juang-10 <63117558+juang-10@users.noreply.github.com>
2021-09-15 07:08:26 -0500

    waiting time while loading pokemon

--- pokeapi-pokedex/src/App.js
+++ pokeapi-pokedex/src/App.js
@@ -9,15 +9,19 @@ const {useState, useEffect} = React;
 
 export default function App() {
   const [pokemons, setPokemons] = useState([]);
+  const [page, setPage] = useState();
+  const [total, setTotal] = useState();
+  const [loading, setLoading] = useState(true);
   
   const fetchPokemons = async () => {
     try {
       const data = await getPokemons();
-      console.log(data.results);
       const promises = data.results.map( async (pokemon) => {
         return await getPokemonData(pokemon.url)
       })
-      const results = await Promise.all()
+      const results = await Promise.all(promises)
+      setPokemons(results)
+      setLoading(false )
     } catch (err) {}
   }
 
@@ -29,7 +33,11 @@ export default function App() {
       <Navbar/>
       <div className="App">
         <Searchbar/>
+        { loading ? (
+          <div>Cargando Pokemones</div>
+        ) : (
         <Pokedex pokemons={pokemons}/>
+        )}
       </div>
     </div>
   );
