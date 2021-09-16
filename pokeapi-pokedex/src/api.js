2d285cbfe45dae605432269c0ec6db527dd5df86
juang-10 <63117558+juang-10@users.noreply.github.com>
2021-09-16 12:27:50 -0500

    get pokemon with a capital letter

--- pokeapi-pokedex/src/api.js
+++ pokeapi-pokedex/src/api.js
@@ -1,5 +1,6 @@
 export const searchPokemon = async (pokemon) => {
     try {
+        pokemon = pokemon.toLowerCase();
         let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
         const response = await fetch(url);
         const data = await response.json();
